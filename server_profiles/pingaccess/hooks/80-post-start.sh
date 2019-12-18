7a49deabae99693b283021ccdf4c94d9aa3a9dc5
ttran <ttran@pingidentity.com>
2019-12-18 23:04:40 +1100

    test standalone fix

---
+++ server_profiles/pingaccess/hooks/80-post-start.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env sh
+#
+# Ping Identity DevOps - Docker Build Hooks
+#
+#- This script is used to import any configurations that are
+#- needed after PingAccess starts
+
+# shellcheck source=pingcommon.lib.sh
+. "${HOOKS_DIR}/pingcommon.lib.sh"
+
+if test -z "${OPERATIONAL_MODE}" || test "${OPERATIONAL_MODE}" = "STANDALONE"  ; then
+  echo "INFO: waiting for PingAccess to start before importing configuration"
+  wait-for localhost:9000 -t 200 --  "${HOOKS_DIR}/81-after-start-process.sh"
+fi
+
+if test "${OPERATIONAL_MODE}" = "CLUSTERED_CONSOLE"  ; then
+  echo "Bringing eth0 back up..."
+  ip link set eth0 up
+fi
